ad6026438ddb694f27343cfe439514e45956cdc1
Daria Jung <djj2115cu@gmail.edu>
2013-01-19 14:01:59 -0500

    added token_generate function

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 import os
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template
 from twilio.util import TwilioCapability
+from credentials import account_sid, auth_token, application_sid
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
+def generate_token():
+    capability = TwilioCapability(account_sid, auth_token)
+    capability.allow_client_outgoing(application_sid)
+    test = capability.generate()
+    print(test)
+    #return capaddbility.generate()
+    return test
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/process', methods=['POST'])
 def process():
@@ -17,9 +24,10 @@ def process():
     r = requests.get(url)
     #process r.content
 
-@app.route('/record')
+@app.route('/record', methods=['GET', 'POST'])
 def record():
-    return render_template('record.html')
+    twilio_token = generate_token() 
+    return render_template('record.html', token = twilio_token)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
