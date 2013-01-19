5fb01be1f3453c2b5850afd5d7ca68fe2902f62d
Dina Lamdany <dinalamdany@gmail.com>
2013-01-19 04:28:50 -0500

    added process route

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 from flask import Flask
+from flask import request
+import requests
 
 app = Flask(__name__)
 
@@ -7,7 +9,14 @@ app = Flask(__name__)
 def hello():
     return 'Hello World!'
 
+@app.route('/process', methods=['POST'])
+def process():
+    url = request.form['RecordingUrl']
+    r = requests.get(url)
+    #process r.content
+
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
