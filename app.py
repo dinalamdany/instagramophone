8c2c469967600cf7e9d87c55e67a113b72ad3ef0
Daria Jung <djj2115cu@gmail.edu>
2013-01-19 04:34:54 -0500

    added twilio to req, created templates, added record route

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from flask import Flask
 from flask import request
-import requests
+from flask import render_template
+from twilio.util import TwilioCapability
 
 app = Flask(__name__)
 
@@ -14,6 +15,10 @@ def process():
     url = request.form['RecordingUrl']
     r = requests.get(url)
     #process r.content
+
+@app.route('/record')
+def record():
+    render_template('record.html')
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
